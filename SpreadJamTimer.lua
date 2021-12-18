1c4fa98bc436fdb8d1fbdf17a104785422624dd1
Dale Williams <dale@dalejwilliams.com>
2021-12-18 23:34:46 +1000

    added tonnes of options

--- SpreadJamTimer.lua
+++ SpreadJamTimer.lua
@@ -11,15 +11,23 @@ obs             = obslua
 APP_NAME        = "SpeadJam"
 APP_VERSION     = "1.0.0"
 SOURCE_NAME 	= "SpreadJam"
+TEXT_PREFIX		= "SJ%dh ║ "
 VID_EXTS		= {"mp4", "mpg", "mkv", "m4v", "mov"}
+ALIGN_CENTER 	= 0
+ALIGN_LEFT		= 1
+ALIGN_RIGHT		= 2
+ALIGN_TOP		= 4
+ALIGN_BOTTOM	= 8
+SIZE_SMALL		= 75
+SIZE_MEDIUM		= 100
+SIZE_LARGE		= 150
+SIZE_HUGE		= 200
 
 duration_hours  = 24;
 text_prefix     = ""
 
 seconds_count = 0
 seconds_total   = 0
-last_text       = ""
-activated       = false
 count_down		= true
 is_recording	= false
 
@@ -30,15 +38,17 @@ config			= {}
 output_path		= nil
 dim_width		= 0
 dim_height		= 0
-
-ALIGN_CENTER 	= 0
-ALIGN_LEFT		= 1
-ALIGN_RIGHT		= 2
-ALIGN_TOP		= 4
-ALIGN_BOTTOM	= 8
+align_h			= ALIGN_LEFT
+align_v			= ALIGN_BOTTOM
+size			= SIZE_MEDIUM
+enabled			= true
 
 -- Function to set the time text
 function update_display()
+	if enabled == false then
+		return
+	end
+
 	-- calculate display seconds
 	local seconds_recorded = 0
 	for k, duration in pairs(durations) do
@@ -71,52 +81,61 @@ function update_display()
 		end
 	end
 
-	if text ~= last_text then
-		-- set data
-		local s = obs.obs_data_create()
-		obs.obs_data_set_string(s, "text", text)
+	-- set data
+	local s = obs.obs_data_create()
+	obs.obs_data_set_string(s, "text", text)
 
-		local dim_min = math.min(dim_height, dim_width)
+	local dim_min = math.min(dim_height, dim_width)
 
-		-- update font		
-		local font = obs.obs_data_create()
-		local font_size = math.ceil(dim_min * 0.04)
-		obs.obs_data_set_string(font, "face", "Arial")
-		obs.obs_data_set_int(font, "size", font_size)
-		obs.obs_data_set_obj(s, "font", font)
-		obs.obs_data_set_int(s, "color", 0xFFFFFFFF)
+	-- update font		
+	local font = obs.obs_data_create()
+	local font_size = math.ceil(dim_min * 0.04 * size * 0.01)
+	obs.obs_data_set_string(font, "face", "Arial")
+	obs.obs_data_set_int(font, "size", font_size)
+	obs.obs_data_set_obj(s, "font", font)
+	obs.obs_data_set_int(s, "color", 0xFFFFFFFF)
 
-		-- get or update source
-		local source = obs.obs_get_source_by_name(SOURCE_NAME)
-		if source == nil then
-			source = obs.obs_source_create("text_gdiplus", SOURCE_NAME, s, nil)
-		else
-			obs.obs_source_update(source, s)
-		end
-
-		-- confirm or add to scene
-		local scene_source = obs.obs_frontend_get_current_scene()
-		local scene = obs.obs_scene_from_source(scene_source)
-		local scene_item = obs.obs_scene_sceneitem_from_source(scene, source)
-		if scene_item == nil then
-			scene_item = obs.obs_scene_add(scene, source)
-		end
-
-		-- position
-		obs.obs_sceneitem_set_order(scene_item, obs.OBS_ORDER_MOVE_TOP)
-		obs.obs_sceneitem_set_locked(scene_item, true)
-		obs.obs_sceneitem_set_alignment(scene_item, bit.bor(ALIGN_LEFT, ALIGN_BOTTOM))
-		local pos = obs.vec2()
-		local pad = math.ceil(dim_min * 0.05)
-		obs.vec2_set(pos, pad, dim_height - pad)
-		obs.obs_sceneitem_set_pos(scene_item, pos)
-
-		-- release
-		obs.obs_source_release(source)
-		obs.obs_data_release(s)
-		obs.obs_data_release(font)
+	-- get or update source
+	local source = obs.obs_get_source_by_name(SOURCE_NAME)
+	if source == nil then
+		source = obs.obs_source_create("text_gdiplus", SOURCE_NAME, s, nil)
+	else
+		obs.obs_source_update(source, s)
 	end
-	last_text = text
+
+	-- confirm or add to scene
+	local scene_source = obs.obs_frontend_get_current_scene()
+	local scene = obs.obs_scene_from_source(scene_source)
+	local scene_item = obs.obs_scene_sceneitem_from_source(scene, source)
+	if scene_item == nil then
+		scene_item = obs.obs_scene_add(scene, source)
+	end
+
+	-- position
+	obs.obs_sceneitem_set_order(scene_item, obs.OBS_ORDER_MOVE_TOP)
+	obs.obs_sceneitem_set_locked(scene_item, true)
+	obs.obs_sceneitem_set_alignment(scene_item, bit.bor(align_h, align_v))
+	local pos = obs.vec2()
+	local pad = math.ceil(dim_min * 0.05)
+	local posx = pad
+	if align_h == ALIGN_CENTER then
+		posx = dim_width * 0.5
+	elseif align_h == ALIGN_RIGHT then
+		posx = dim_width - pad
+	end
+	local posy = pad
+	if align_v == ALIGN_CENTER then
+		posy = dim_height * 0.5
+	elseif align_v == ALIGN_BOTTOM then
+		posy = dim_height - pad
+	end
+	obs.vec2_set(pos, posx, posy)
+	obs.obs_sceneitem_set_pos(scene_item, pos)
+
+	-- release
+	obs.obs_source_release(source)
+	obs.obs_data_release(s)
+	obs.obs_data_release(font)
 end
 
 function timer_callback()
@@ -131,12 +150,29 @@ end
 function script_properties()
 	local props = obs.obs_properties_create()
 
-    local f = obs.obs_properties_add_list(props, "hours", "Hours", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
-	obs.obs_property_list_add_int(f, "Full 24h", 24)
-    obs.obs_property_list_add_int(f, "Mini 12h", 12)
+    local o_hours = obs.obs_properties_add_list(props, "hours", "Jam Hours (Duration)", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
+	obs.obs_property_list_add_int(o_hours, "Full 24h", 24)
+    obs.obs_property_list_add_int(o_hours, "Mini 12h", 12)
+
+	local o_align_h = obs.obs_properties_add_list(props, "align_h", "Position Horizontal", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
+	obs.obs_property_list_add_int(o_align_h, "Left", ALIGN_LEFT)
+    obs.obs_property_list_add_int(o_align_h, "Center", ALIGN_CENTER)
+    obs.obs_property_list_add_int(o_align_h, "Right", ALIGN_RIGHT)
+
+	local o_align_v = obs.obs_properties_add_list(props, "align_v", "Position Vertical", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
+	obs.obs_property_list_add_int(o_align_v, "Top", ALIGN_TOP)
+    obs.obs_property_list_add_int(o_align_v, "Middle", ALIGN_CENTER)
+    obs.obs_property_list_add_int(o_align_v, "Bottom", ALIGN_BOTTOM)
+
+	local o_size = obs.obs_properties_add_list(props, "size", "Display Size", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
+    obs.obs_property_list_add_int(o_size, "Medium", SIZE_MEDIUM)
+	obs.obs_property_list_add_int(o_size, "Small", SIZE_SMALL)
+	obs.obs_property_list_add_int(o_size, "Large", SIZE_LARGE)
+	obs.obs_property_list_add_int(o_size, "Huge", SIZE_HUGE)
 
 	obs.obs_properties_add_text(props, "text_prefix", "Prefix", obs.OBS_TEXT_DEFAULT)
 	obs.obs_properties_add_bool(props, "count_down", "Count Down")
+	obs.obs_properties_add_bool(props, "enabled", "Enabled")
 
 	return props
 end
@@ -153,14 +189,23 @@ function script_update(settings)
     duration_hours = obs.obs_data_get_int(settings, "hours")
 	seconds_total = duration_hours * 60 * 60
     text_prefix = obs.obs_data_get_string(settings, "text_prefix")
+	align_h = obs.obs_data_get_int(settings, "align_h")
+	align_v = obs.obs_data_get_int(settings, "align_v")
+	size = obs.obs_data_get_int(settings, "size")
 	count_down = obs.obs_data_get_bool(settings, "count_down")
+	enabled = obs.obs_data_get_bool(settings, "enabled")
+	update_display()
 end
 
 -- A function named script_defaults will be called to set the default settings
 function script_defaults(settings)
 	obs.obs_data_set_default_int(settings, "hours", 24)
-    obs.obs_data_set_default_string(settings, "text_prefix", "SJ%dh ║ ")
+    obs.obs_data_set_default_string(settings, "text_prefix", TEXT_PREFIX)
+	obs.obs_data_set_default_int(settings, "align_h", ALIGN_LEFT)
+	obs.obs_data_set_default_int(settings, "align_v", ALIGN_BOTTOM)
+	obs.obs_data_set_default_int(settings, "size", SIZE_MEDIUM)
     obs.obs_data_set_default_bool(settings, "count_down", true)
+    obs.obs_data_set_default_bool(settings, "enabled", true)
 end
 
 -- A function named script_save will be called when the script is saved
@@ -225,7 +270,6 @@ function calculate_recorded_update()
 				-- try to read the duration
 				duration = obs.obs_source_media_get_duration(info.source)
 				if duration > 0 or info.attempts > 4 then
-					--print("calculated " .. duration .. " for " .. filepath)
 					obs.obs_source_release(info.source)
 					info.source = nil
 					durations[filepath] = duration / 1000
@@ -236,7 +280,6 @@ function calculate_recorded_update()
 				end
 			else
 				-- create a source
-				--print("calculating " .. filepath .. "...")
 				info.source = obs.obs_source_create_private("ffmpeg_source", "Global Media Source", nil)
 				local s = obs.obs_data_create()
 				obs.obs_data_set_string(s, "local_file", filepath)
@@ -303,7 +346,7 @@ function load_config()
 		end
 	end
 	output_path = config["SimpleOutput.FilePath"]
-	local res = config["RecRescaleRes"]
+	local res = config["FFRescaleRes"] or config["RescaleRes"]
 	if res ~= nil then
 		local xi = string.find(res, 'x')
 		if xi > 0 then
